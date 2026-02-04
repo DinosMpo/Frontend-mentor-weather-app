89e7823ab40b3e0711cdbb1f0913d58ab2ac6fac
DinosMpo <dinos7591@yahoo.gr>
2026-02-04 15:43:00 +0200

    search in progress component

---
+++ app/comps/SearchInProgress/SearchInProgress.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import  "./SearchInProgress.css"
+
+export default function SearchInProgress() {
+  return (
+    <div id="search-in-progress">
+        <img src="./icon-loading.svg" alt="search in progress"/>
+        Search in progress
+    </div>
+  )
+}
